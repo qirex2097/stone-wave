b6fbc315e3da8b1efafcf2b81dd814f49f8b81a1
Masaru Hori <qirex_rr@yahoo.co.jp>
2024-10-13 15:12:51 +0900

    draw_lineを追加

--- main.c
+++ main.c
@@ -20,6 +20,53 @@ void my_mlx_pixel_put(t_data *data, int x, int y, int color)
     *(unsigned int *)dst = color;
 }
 
+void draw_line(t_data *data, int x0, int y0, int x1, int y1, int color)
+{
+    int dx, dy;
+    int sx, sy;
+    int err;
+
+    if (x0 < x1)
+    {
+        dx = x1 - x0;
+        sx = 1;
+    }
+    else
+    {
+        dx = x0 - x1;
+        sx = -1;
+    }
+    if (y0 < y1)
+    {
+        dy = y1 - y0;
+        sy = 1;
+    }
+    else
+    {
+        dy = y0 - y1;
+        sy = -1;
+    }
+    err = dx - dy;
+
+    while (x0 != x1 && y0 != y1)
+    {
+        int e2;
+
+        my_mlx_pixel_put(data, x0, y0, color);
+        e2 = 2 * err;
+        if (e2 > -dy)
+        {
+            err -= dy;
+            x0 += sx;
+        }
+        if (e2 < dx)
+        {
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
+
 int main(void)
 {
     void *mlx;
@@ -38,6 +85,7 @@ int main(void)
     data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
 
     my_mlx_pixel_put(&data, 5, 5, 0x00FF0000);
+    draw_line(&data, 5, 5, WINDOW_W - 5, WINDOW_H - 5, 0x0000ff00);
     mlx_put_image_to_window(mlx, mlx_win, data.img, 0, 0);
 
     mlx_loop(mlx);
