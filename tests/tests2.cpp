53d17d50cdbb346f222f62984700e702d33929b6
Masaru Hori <qirex_rr@yahoo.co.jp>
2024-10-30 04:09:04 +0900

    fint_next_grid_crossingのテストの修正

--- tests/tests2.cpp
+++ tests/tests2.cpp
@@ -5,30 +5,33 @@ extern "C"
 #include "../srcs/stone.h" // テスト対象のヘッダー
 }
 
-TEST(MyLibraryTest2, FunctionReturnsExpectedValue)
+TEST(FindNextGridCrossingTest, FunctionReturnsExpectedValue)
 {
-    t_map *map = init_map(10, 10);
+    int grid_size = 100;
     t_pos player_pos = {10, 20};
     t_vec dir = {3, 1};
     t_pos cross_point;
-    // void ray_grid_intersection(t_map *map, t_pos ray_origin, t_vec ray_direction)
-    cross_point = find_next_grid_crossing(player_pos, dir, map->grid_size);
+    cross_point = find_next_grid_crossing(player_pos, dir, grid_size);
+    EXPECT_EQ(cross_point.x, 100);
     player_pos.x = 150;
     player_pos.y = 20;
     dir.x = -3;
     dir.y = 1;
-    cross_point = find_next_grid_crossing(player_pos, dir, map->grid_size);
+    cross_point = find_next_grid_crossing(player_pos, dir, grid_size);
+    EXPECT_EQ(cross_point.x, 100);
     player_pos.x = 100;
     player_pos.y = 920;
     dir.x = 0;
     dir.y = -1;
-    cross_point = find_next_grid_crossing(player_pos, dir, map->grid_size);
+    cross_point = find_next_grid_crossing(player_pos, dir, grid_size);
+    EXPECT_EQ(cross_point.y, 900);
     player_pos.x = 550;
     player_pos.y = 550;
     dir.x = -1;
     dir.y = -1;
-    cross_point = find_next_grid_crossing(player_pos, dir, map->grid_size);
-    free(map);
+    cross_point = find_next_grid_crossing(player_pos, dir, grid_size);
+    EXPECT_EQ(cross_point.x, 500);
+    EXPECT_EQ(cross_point.y, 500);
 }
 
 TEST(BuffTest, Buff)
