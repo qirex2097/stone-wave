d8b97af214d1610da70b4fcff36f817fbf0ed3e6
Masaru Hori <qirex_rr@yahoo.co.jp>
2024-11-02 09:01:45 +0900

    draw_player_view リファクタリング

--- srcs/mini_window.c
+++ srcs/mini_window.c
@@ -75,39 +75,33 @@ int calculate_line_length(t_pos p, int angle, t_pos cross_point)
     return (int)(WALL_HEIGHT / distance);
 }
 
+t_vec calc_view_direction(t_pos p0, t_line screen_line, int width, int sx)
+{
+    t_pos p1;
+    p1 = map_point_on_line(screen_line, width, sx);
+    return (t_vec){p1.x - p0.x, p1.y - p0.y};
+}
+
 void draw_player_view(t_vars *vars, t_line screen_line)
 {
     t_player *player = &vars->player;
+    t_pos player_pos = (t_pos){player->x, player->y};
 
     int sx = 0;
     while (sx < vars->img2.w)
     {
-        t_line view_ray;
-        t_pos p1;
-        int width = vars->img2.w;
-        p1 = map_point_on_line(screen_line, width, sx);
-        view_ray.x0 = player->x;
-        view_ray.y0 = player->y;
-        view_ray.x1 = p1.x;
-        view_ray.y1 = p1.y;
-        // draw_line(&vars->img, &vars->camera, &view_ray, 0x00ffffff);
-
-        t_pos origin, cross_point;
         t_vec direction;
+        t_pos cross_point;
+        int color, line_length;
 
-        double radian = (player->angle * PI) / 180.0;
-        origin.x = player->x;
-        origin.y = player->y;
-        direction.x = view_ray.x1 - view_ray.x0;
-        direction.y = view_ray.y1 - view_ray.y0;
-        cross_point = detect_ray_wall_intersection(vars->map, origin, direction);
+        direction = calc_view_direction(player_pos, screen_line, vars->img2.w, sx);
+        cross_point = detect_ray_wall_intersection(vars->map, player_pos, direction);
         draw_circle(&vars->img, &vars->camera, &cross_point, 3, 0x00ffffff);
 
-        int color;
         color = get_wall_color(vars->map, cross_point, direction);
 
         // draw a line at position sx
-        int line_length = calculate_line_length(origin, (player->angle + 90) % 360, cross_point);
+        line_length = calculate_line_length(player_pos, (player->angle + 90) % 360, cross_point);
         if (line_length > 0)
             my_mlx_draw_line(&vars->img2,
                              sx, MINIWINDOW_H / 2 - line_length / 2,
