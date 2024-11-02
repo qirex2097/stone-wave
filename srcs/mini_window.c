eb6e96bec37d73e4bf615e4fbca4ff214c4197bf
Masaru Hori <qirex_rr@yahoo.co.jp>
2024-11-02 10:22:47 +0900

    draw_player_view リファクタリング

--- srcs/mini_window.c
+++ srcs/mini_window.c
@@ -75,17 +75,21 @@ int calculate_line_length(t_pos p, int angle, t_pos cross_point)
     return (int)(WALL_HEIGHT / distance);
 }
 
-t_vec calc_view_direction(t_pos p0, t_line screen_line, int width, int sx)
+t_vec calc_view_direction(const t_pos p0, t_line screen_line, int width, int sx)
 {
     t_pos p1;
     p1 = map_point_on_line(screen_line, width, sx);
     return (t_vec){p1.x - p0.x, p1.y - p0.y};
 }
 
+#define INTERSECTION_POINT_RADIUS 3
 void draw_player_view(t_vars *vars, t_line screen_line)
 {
     t_player *player = &vars->player;
-    t_pos player_pos = (t_pos){player->x, player->y};
+    const t_pos player_pos = (t_pos){player->x, player->y};
+    const int perpendicular_angle = (player->angle + 90) % 360;
+    const int width = vars->img2.w;
+    const int half_height = MINIWINDOW_H / 2;
 
     int sx = 0;
     while (sx < vars->img2.w)
@@ -94,18 +98,18 @@ void draw_player_view(t_vars *vars, t_line screen_line)
         t_pos cross_point;
         int color, line_length;
 
-        direction = calc_view_direction(player_pos, screen_line, vars->img2.w, sx);
+        direction = calc_view_direction(player_pos, screen_line, width, sx);
         cross_point = detect_ray_wall_intersection(vars->map, player_pos, direction);
-        draw_circle(&vars->img, &vars->camera, &cross_point, 3, 0x00ffffff);
+        draw_circle(&vars->img, &vars->camera, &cross_point, INTERSECTION_POINT_RADIUS, 0x00ffffff);
 
         color = get_wall_color(vars->map, cross_point, direction);
 
         // draw a line at position sx
-        line_length = calculate_line_length(player_pos, (player->angle + 90) % 360, cross_point);
+        line_length = calculate_line_length(player_pos, perpendicular_angle, cross_point);
         if (line_length > 0)
             my_mlx_draw_line(&vars->img2,
-                             sx, MINIWINDOW_H / 2 - line_length / 2,
-                             sx, MINIWINDOW_H / 2 + line_length / 2, color);
+                             sx, half_height - line_length / 2,
+                             sx, half_height + line_length / 2, color);
         sx++;
     }
 }
